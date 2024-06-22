0abd828d66c0ffd6d5b97e00d81ede0016e65c39
noumanmani <168138758+noumanmani@users.noreply.github.com>
2024-06-22 20:48:14 +0500

    project is complete

--- navbar.js
+++ navbar.js
@@ -2,3 +2,7 @@ function showSidebar(){
     const side =document.querySelector(".side-bar")
     side.style.display="flex" 
  }
+function hidesidebar(){
+    const side =document.querySelector(".side-bar")
+    side.style.display="none" 
+}
